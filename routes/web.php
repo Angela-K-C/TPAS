5a19d1e602952b6b5a36a4fa85e7a2f019130afd
Robert <mndungu.robert@gmail.com>
2025-11-07 15:25:50 +0300

    Admin Routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Method, route name, Callback/Controller (basically a function), Action
+// get/post
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('admin-login', function(){
+    return view('adminPages.login');
+});
+
+Route::get('admin-dashboard', function(){
+    return view('adminPages.dashboard');
+});
+
+Route::get('manage-users', function(){
+    return view('adminPages.manageUsers');
 });
